656a08748d9b3078d848574ae2c38f46ea40deca
Nobutann <luizgonzagafilho201365@gmail.com>
2025-06-21 15:15:56 -0300

    adicionado função de excluir tarefas

--- todoapp/to_do/urls.py
+++ todoapp/to_do/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('uptade-task/<str:pk>/', views.updateTask, name="update-task"),
+    path('delete-task/<str:pk>/', views.deleteTask, name="delete-task"),
 ]
