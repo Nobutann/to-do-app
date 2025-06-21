f618caf626746c111ae6c2e51b71532a93161d3c
Nobutann <luizgonzagafilho201365@gmail.com>
2025-06-21 10:26:35 -0300

    criado django forms

--- todoapp/to_do/views.py
+++ todoapp/to_do/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Task
+from .forms import TaskForm
 
 def index(request):
+    form = TaskForm()
     tasks = Task.objects.all() 
-    context = {'tasks': tasks}
+    context = {'tasks': tasks, 'TaskForm': form}
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            
+        return redirect('/')
 
     return render(request, 'index.html', context)
